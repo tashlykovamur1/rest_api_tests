afdbe44c5676a1f90be0584e7b6773747f58eb2c
a.tashlykova <a.tashlykova@cyrm.ru>
2024-10-26 22:21:35 +0500

    Реализовала форматирование респонса

--- tests/account/test_get_v1_account.py
+++ tests/account/test_get_v1_account.py
@@ -1,4 +1,13 @@
+from hamcrest import assert_that, equal_to
+
+
 def test_get_v1_account(auth_account_helper):
-    helper, _ = auth_account_helper
+    helper, user_data = auth_account_helper
+
     response = helper.dm_account_api.account_api.get_v1_account()
-    assert response.status_code == 200, f'Не удалось получить пользователя, {response.json()}'
+
+    assert_that(response.status_code, equal_to(200),
+                f'Не удалось получить пользователя, {response.body, response.error}')
+    assert_that(response.body['resource']['login'], equal_to(user_data.login))
+    assert_that(response.body['resource']['rating']['enabled'], equal_to(True))
+
